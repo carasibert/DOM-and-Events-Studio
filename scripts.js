27bcf75f5a86f2f734640f1176d0951f67784837
carasibert <47763369+carasibert@users.noreply.github.com>
2023-07-13 18:52:50 -0500

    added the takeoff functionality to add a window confirm, set flight status, change background color to blue, and increase shuttle height by 10,000 miles

--- scripts.js
+++ scripts.js
@@ -1,2 +1,23 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener('load', function() {
+    let takeoffButton = document.getElementById('takeoff');
+    let flightStatus = document.getElementById('flightStatus');
+    let shuttleBackground = document.getElementById('shuttleBackground');
+    let spaceShuttleHeightElement = document.getElementById('spaceShuttleHeight');
+
+    takeoffButton.addEventListener('click', ()=> {
+        let readyForTakeoff = confirm("Confirm that the shuttle is ready for takeoff.");
+
+        if (readyForTakeoff) {
+            flightStatus.innerHTML = 'Flight status: Shuttle in flight';
+            shuttleBackground.style.backgroundColor = 'blue';
+    
+            let spaceShuttleHeight = 0;
+            spaceShuttleHeight += 10000;
+            spaceShuttleHeightElement.innerHTML = spaceShuttleHeight;
+            console.log(`Shuttle height: ${spaceShuttleHeight} miles`);
+            };
+        });
+    });
