51c03516b0f9bc57eb0e9d649e6844f1140ce7c6
carasibert <47763369+carasibert@users.noreply.github.com>
2023-07-13 19:31:37 -0500

    latest refactor

--- scripts.js
+++ scripts.js
@@ -2,22 +2,31 @@
 // Remember to pay attention to page loading!
 
 window.addEventListener('load', function() {
-    let takeoffButton = document.getElementById('takeoff');
-    let flightStatus = document.getElementById('flightStatus');
-    let shuttleBackground = document.getElementById('shuttleBackground');
-    let spaceShuttleHeightElement = document.getElementById('spaceShuttleHeight');
+    const takeoffButton = document.getElementById('takeoff');
+    const flightStatusParagraph = document.getElementById('flightStatus');
+    const spaceShuttleBackground = document.getElementById('shuttleBackground');
+    const spaceShuttleHeightElement = document.getElementById('spaceShuttleHeight');
+    const landButton = document.getElementById('landing');
+    const missionAbortButton = document.getElementById('missionAbort');
+    const upButton = document.getElementById('up');
+    const downButton = document.getElementById('down');
+    const rightButton = document.getElementById('right');
+    const leftButton = document.getElementById('left');
 
-    takeoffButton.addEventListener('click', ()=> {
-        let readyForTakeoff = confirm("Confirm that the shuttle is ready for takeoff.");
+    takeoffButton.addEventListener('click', function(){
+        let readyForTakeoff = window.confirm("Confirm that the shuttle is ready for takeoff.");
 
         if (readyForTakeoff) {
-            flightStatus.innerHTML = 'Flight status: Shuttle in flight';
-            shuttleBackground.style.backgroundColor = 'blue';
+            flightStatusParagraph.innerHTML = 'Flight status: Shuttle in flight';
+            spaceShuttleBackground.style.backgroundColor = 'blue';
     
             let spaceShuttleHeight = 0;
             spaceShuttleHeight += 10000;
             spaceShuttleHeightElement.innerHTML = spaceShuttleHeight;
-            console.log(`Shuttle height: ${spaceShuttleHeight} miles`);
             };
         });
+
+    landButton.addEventListener('click', function(){
+        let landingEngaged = window.alert("The shuttle is landing. Landing gear engaged.");
+    })
     });
