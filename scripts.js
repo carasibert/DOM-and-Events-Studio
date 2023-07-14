674f0c22e1d39c353146843112e6cff1a9f1b028
carasibert <47763369+carasibert@users.noreply.github.com>
2023-07-13 21:18:17 -0500

    added land, takeoff, up, down, right, left, abort mission functionality, and prevented user from going above the page

--- scripts.js
+++ scripts.js
@@ -12,6 +12,11 @@ window.addEventListener('load', function() {
     const downButton = document.getElementById('down');
     const rightButton = document.getElementById('right');
     const leftButton = document.getElementById('left');
+    let shuttleAlt = Number(spaceShuttleHeightElement.innerHTML);
+    let shuttleImage = document.getElementById('rocket');
+    shuttleImage.style.position = 'relative';
+    shuttleImage.style.left = '0px';
+    shuttleImage.style.top = '250px';
 
     takeoffButton.addEventListener('click', function(){
         let readyForTakeoff = window.confirm("Confirm that the shuttle is ready for takeoff.");
@@ -23,10 +28,63 @@ window.addEventListener('load', function() {
             let spaceShuttleHeight = 0;
             spaceShuttleHeight += 10000;
             spaceShuttleHeightElement.innerHTML = spaceShuttleHeight;
+            shuttleImage.style.top = "240px";
+            shuttleImage.style.left = "0px";
             };
         });
 
     landButton.addEventListener('click', function(){
-        let landingEngaged = window.alert("The shuttle is landing. Landing gear engaged.");
-    })
+        window.alert('The shuttle is landing. Landing gear engaged.');
+        flightStatusParagraph.innerHTML = 'The shuttle has landed.';
+        spaceShuttleBackground.style.backgroundColor = 'green';
+        shuttleAlt = 0;
+        spaceShuttleHeightElement.innerHTML = shuttleAlt;
+        shuttleImage.style.bottom = "0px";
+        shuttleImage.style.top = "250px";
+        shuttleImage.style.left = "0px";
     });
+
+    missionAbortButton.addEventListener('click', function(){
+        let abortConfirm = window.confirm('Confirm that you want to abort the mission.');
+
+        if (abortConfirm) {
+            flightStatusParagraph.innerHTML = 'Mission aborted';
+            spaceShuttleBackground.style.backgroundColor = 'green';
+            shuttleAlt = 0;
+            spaceShuttleHeightElement.innerHTML = shuttleAlt;
+            shuttleImage.style.bottom = "0px";
+            shuttleImage.style.top = "250px";
+            shuttleImage.style.left = "0px";
+        }
+    });
+
+    upButton.addEventListener('click', function(){
+        let currentHeight = parseInt(shuttleImage.style.top);
+        if (currentHeight <= 0) {
+            shuttleImage.style.top = currentHeight
+        } else {
+        shuttleImage.style.top = (currentHeight - 10) + "px";
+        shuttleAlt += 10000;
+        }
+        spaceShuttleHeightElement.innerHTML = shuttleAlt;
+    });
+
+    downButton.addEventListener('click', function(){
+        let currentHeight = parseInt(shuttleImage.style.top);
+        shuttleImage.style.top = (currentHeight + 10) + "px";
+        shuttleAlt -= 10000;
+        spaceShuttleHeightElement.innerHTML = shuttleAlt;
+
+    });
+
+    leftButton.addEventListener('click', function(){
+        let currentWidth = parseInt(shuttleImage.style.left);
+        shuttleImage.style.left = (currentWidth - 10) + "px";
+    });
+
+    rightButton.addEventListener('click', function(){
+        let currentWidth = parseInt(shuttleImage.style.left);
+        shuttleImage.style.left = (currentWidth + 10) + "px";
+    });
+});
+
